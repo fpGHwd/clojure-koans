5dccdb08e3ee2aff68a9c728e67c6e47a5ed94b3
wangding02 <wangding02@saicmotor.com>
2025-01-10 20:57:57 +0800

    accomplish functions exercises

--- src/koans/07_functions.clj
+++ src/koans/07_functions.clj
@@ -26,16 +26,18 @@
   (= "AACC" (#(str "AA" %2) "bb" "CC"))
 
   "One function can beget another"
-  (= 9 (((fn [] ___)) 4 5))
+  (= 9 (((fn [] +)) 4 5))
   ;; 这个问题完全不知道要干什么，我只知道需要补充一个函数，但是括号多了一个。。。
 
   "Functions can also take other functions as input"
   (= 20 ((fn [f] (f 4 5))
-         ___))
+         *))
 
   "Higher-order functions take function arguments"
-  (= 25 (___
+  (= 25 ((fn [f] (f 5))
          (fn [n] (* n n))))
 
   "But they are often better written using the names of functions"
-  (= 25 (___ square)))
+  (= 25 ((fn [f] (f 5)) square)))
+
+;; 可以，几乎是自己独立做完的，感觉太有意思了哈哈哈。人生的乐趣就是靠自己去体会的，没有人能替你体会哈哈哈哈。乐趣纯来自于自己。
