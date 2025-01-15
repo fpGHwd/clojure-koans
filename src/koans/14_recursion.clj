83875c2e8a82afc3f439890564345d5645b458cf
Wang Ding <ggwdwhu@gmail.com>
2025-01-15 20:55:27 +0800

    accomplish recursion exercise

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,21 +3,31 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
+
 (defn recursive-reverse [coll]
-  __)
+  (loop [coll coll
+         acc ()]
+    (if (seq coll)
+      (recur (rest coll) (conj acc (first coll)))
+      acc)))
 
 (defn factorial [n]
-  __)
+  (loop [n n
+         ret 1]
+    (if (> n 1)
+      (recur (dec n) (* ret n))
+      ret)))
+;; 亲自做对的，看来自己已经抓到了精髓了哈，开心。 2025/1/15
 
 (meditations
   "Recursion ends with a base case"
