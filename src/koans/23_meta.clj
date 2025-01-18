377fd5e482cc625dd9347404caed460e959cc220
Wang Ding <ggwdwhu@gmail.com>
2025-01-18 12:53:39 +0800

    accomplish meta exercises

--- src/koans/23_meta.clj
+++ src/koans/23_meta.clj
@@ -15,38 +15,39 @@
 
   "While others can't"
   (= "This doesn't implement the IObj interface" (try
-          (with-meta
-            2
-            {:prime true})
-          (catch ClassCastException e
-            "This doesn't implement the IObj interface")))
+                                                   (with-meta
+                                                     2
+                                                     {:prime true})
+                                                   (catch ClassCastException e
+                                                     "This doesn't implement the IObj interface")))
 
   "Notice when metadata carries over"
-  (= __ (meta (merge '^{:foo :bar} {:a 1 :b 2}
-                     {:b 3 :c 4})))
+  (= {:foo :bar} (meta (merge '^{:foo :bar} {:a 1 :b 2}
+                              {:b 3 :c 4})))
 
   "And when it doesn't"
-  (= __ (meta (merge {:a 1 :b 2}
-                     '^{:foo :bar} {:b 3 :c 4})))
+  (= nil (meta (merge {:a 1 :b 2}
+                      '^{:foo :bar} {:b 3 :c 4})))
 
   "Metadata can be used as a type hint to avoid reflection during runtime"
-  (= __ (#(.charAt ^String % 0) "Cast me"))
+  (= \C (#(.charAt ^String % 0) "Cast me"))
 
   "You can directly update an object's metadata"
   (= 8 (let [giants
              (with-meta
                'Giants
                {:world-series-titles (atom 7)})]
-         (swap! (:world-series-titles (meta giants)) __)
+         (swap! (:world-series-titles (meta giants)) inc)
          @(:world-series-titles (meta giants))))
+  ;; 这个代码有什么问题吗
 
   "You can also create a new object from another object with metadata"
   (= {:league "National League" :park "Oracle Park"}
      (meta (vary-meta giants
-                      assoc __ __)))
+                      assoc :park "Oracle Park")))
 
   "But it won't affect behavior like equality"
-  (= __ (vary-meta giants dissoc :league))
+  (= 'Giants (vary-meta giants dissoc :league))
 
   "Or the object's printed representation"
-  (= __ (pr-str (vary-meta giants dissoc :league))))
+  (= "Giants" (pr-str (vary-meta giants dissoc :league))))
